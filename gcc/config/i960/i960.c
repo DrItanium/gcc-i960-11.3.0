852ca20139588975bb2ac98f4b06e14ec7d7b023
Joshua Scoggins <Theoretically.x64@gmail.com>
2022-08-08 11:00:13 -0700

    Make sure we are using local labels and removed forced names in some places

--- gcc/config/i960/i960.c
+++ gcc/config/i960/i960.c
@@ -1178,7 +1178,7 @@ i960_function_name_declare (FILE* file, const char* name, tree fndecl)
       assemble_name (file, name);
       fprintf (file, ",%s.lf\n", (name[0] == '*' ? &name[1] : name));
       ASM_OUTPUT_LABEL (file, name);
-      fprintf (file, "\tlda    Li960R%d,g14 #lda4\n", ret_label);
+      fprintf (file, "\tlda    .Li960R%d,g14 #lda4\n", ret_label);
       fprintf (file, "%s.lf:\n", (name[0] == '*' ? &name[1] : name));
       fprintf (file, "\tmov    g14,g%d\n", i960_leaf_ret_reg);
 #if 0
@@ -1586,7 +1586,7 @@ i960_output_function_epilogue (FILE* file/*, HOST_WIDE_INT size*/)
 {
   if (i960_leaf_ret_reg >= 0)
     {
-      fprintf (file, "Li960R%d:	ret\n", ret_label);
+      fprintf (file, ".Li960R%d:	ret\n", ret_label);
       return;
     }
 
@@ -1616,11 +1616,11 @@ i960_output_function_epilogue (FILE* file/*, HOST_WIDE_INT size*/)
 	    }
 	  break;
 	}
-      fprintf (file, "Li960R%d:	ret\n", ret_label);
+      fprintf (file, ".Li960R%d:	ret\n", ret_label);
       return;
     }
 
-  fprintf (file, "Li960R%d:\n", ret_label);
+  fprintf (file, ".Li960R%d:\n", ret_label);
 
   fprintf (file, "\t#EPILOGUE#\n");
 
@@ -1703,7 +1703,7 @@ i960_output_ret_insn (rtx_insn* insn)
       if (! TARGET_CODE_ALIGN && next_real_insn (insn) == 0)
 	return "";
 
-      sprintf (lbuf, "b	Li960R%d", ret_label);
+      sprintf (lbuf, "b	.Li960R%d", ret_label);
       return lbuf;
     }
 
