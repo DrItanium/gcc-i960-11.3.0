9f7a405e13b1f1f243e6fac22d1fc8f00deacdfa
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-04-13 16:35:48 -0700

    Keep defining the minimum set of operations necessary
    Got the register set defined with a complete description which makes me
    happier.

--- gcc/config/i960/i960.h
+++ gcc/config/i960/i960.h
@@ -22,4 +22,62 @@
 
 /* Width of a word, in units (bytes).  */
 #define UNITS_PER_WORD 4
+
+/* 
+ * Register map:
+ * g0-g15, pfp, sp, rip, r4-r15, fp0-3, cc, ac, pc, tc, sf0-sf31
+ *
+ * Registers with a fixed usage:
+ * g14, g15, pfp, sp, rip, cc, ac, pc, tc, sf0-sf31
+ *
+ * The special function registers are listed there for consistency and not
+ * expected to be used at all
+ */
+
+#define FIXED_REGISTERS \
+ {0, 0, 0, 0, 0, 0, 0, 0,	\
+  0, 0, 0, 0, 0, 0, 1, 1,	\
+  1, 1, 1, 0, 0, 0, 0, 0,	\
+  0, 0, 0, 0, 0, 0, 0, 0,	\
+  0, 0, 0, 0, 1, 1, 1, 1,   \
+  1, 1, 1, 1, 1, 1, 1, 1,   \
+  1, 1, 1, 1, 1, 1, 1, 1,   \
+  1, 1, 1, 1, 1, 1, 1, 1,   \
+  1, 1, 1, 1, 1, 1, 1, 1}
+
+/* the first psuedo register is #72 in this new port */
+#define FIRST_PSEUDO_REGISTER 72
+
+/* Taken from previous impl since it describes the calling convention with
+ * some additions:
+	g0..g3 are used for return values,
+	g0..g7 may always be used for parameters,
+	g8..g11 may be used for parameters, but are preserved if they aren't,
+	g12 is the static chain if needed, otherwise is preserved
+	g13 is the struct return ptr if used, or temp, but may be trashed,
+	g14 is the leaf return ptr or the arg block ptr otherwise zero,
+		must be reset to zero before returning if it was used,
+	g15 is the frame pointer,
+	r0 is the previous FP,
+	r1 is the stack pointer,
+	r2 is the return instruction pointer,
+	r3-r15 are always available,
+	r3 is clobbered by calls in functions that use the arg pointer
+	r4-r11 may be clobbered by the mcount call when profiling
+	r4-r15 if otherwise unused may be used for preserving global registers
+	fp0-fp3 are never available.  
+    cc, ac, pc, tc are never available.
+    sf0-sf31 are never available.
+    */
+
+#define CALL_USED_REGISTERS \
+ {1, 1, 1, 1, 1, 1, 1, 1,	\
+  0, 0, 0, 0, 0, 0, 1, 1,	\
+  1, 1, 1, 0, 0, 0, 0, 0,	\
+  0, 0, 0, 0, 0, 0, 0, 0,	\
+  1, 1, 1, 1, 1, 1, 1, 1,   \
+  1, 1, 1, 1, 1, 1, 1, 1,   \
+  1, 1, 1, 1, 1, 1, 1, 1,   \
+  1, 1, 1, 1, 1, 1, 1, 1,   \
+  1, 1, 1, 1, 1, 1, 1, 1}
 #endif
