25c28f75856f75d31b7314c63179c51fbac0a3c4
Joshua Scoggins <Theoretically.x64@gmail.com>
2026-02-14 14:37:47 -0800

    Define the different cpu targets

--- gcc/config/i960/i960-opts.h
+++ gcc/config/i960/i960-opts.h
@@ -24,4 +24,71 @@
 
 #ifndef I960_OPTS_H
 #define I960_OPTS_H
+// floating point abi types
+enum i960_float_abi_type {
+    FLOAT_ABI_SOFT,
+    FLOAT_ABI_HARD
+};
+
+enum i960_processor_type {
+    // these cores are based off of the original design (with the exception of
+    // the i960SA processor) so really they are the same core and accept the
+    // same set of options. The problem though is that the extended
+    // architecture cannot be used on the S series because of a lack of tag
+    // pin.
+    //
+    // The K and M series probably has the tag pin via omission of
+    // documentation. It is important to note that this does not really help us
+    // at all!
+    ARCH_KA, // K-series, no FPU, no MMU, 32-bit data bus (although FPU and MMU are there)
+    ARCH_KB, // K-series, FPU, no MMU, 32-bit data bus (although MMU is there)
+    ARCH_KC, // K-series, FPU, MMU, 32-bit data bus (theoretical)
+    ARCH_SA, // S-series, no FPU, no MMU, 16-bit data bus
+    ARCH_SB, // S-series, FPU, no MMU, 16-bit data bus (although MMU is in there)
+    ARCH_SC, // S-series, FPU, MMU, 16-bit data bus (theoretical and never
+             // released)
+    ARCH_MC, // M-series, FPU, MMU, 32-bit data bus
+    ARCH_XA, // X-series, FPU, MMU, Extended ISA 32-bit data bus (not supported but referenced here)
+    // C-series processors. These processors are superscalar i960s with a new
+    // core that does not support FPU or MMU operations. Does introduce a DMA
+    // instructions.
+    ARCH_CA, // C-series, DMA, Core
+    ARCH_CF, // C-series
+    // J-Series processors take the C-series core and improve on it by reducing
+    // power consumption. Various different models, does not support DMA but
+    // does have new core instructions that are of the vein of cmpob and cmpib
+    // but with moves (conditional select), adds (conditional add), and
+    // conditional subtraction. These processors are meant to be used as
+    // microcontrollers. Branch predicition hints are supported
+    ARCH_JA, // 
+    ARCH_JC, // J-series (no MMU)
+    ARCH_JD, // J-series
+    ARCH_JF,
+    ARCH_JS,
+    ARCH_JT,
+    // H-Series Processors
+    // A further evolution of the J series with improved performance and some
+    // builtin features like timers. 
+    ARCH_HA,
+    ARCH_HD,
+    ARCH_HT,
+    // RX I/O processor, not much known about it but it is basically an i960
+    // with a PCI peripheral attached to it. Not really supported. Both this
+    // and the V series have mentions in the intel compiler manual that the
+    // processor has gone hardcore risc and dropped duplicate instructions.
+    ARCH_RX,
+    // V series, not much known about it but it seems to have gone hardcore
+    // risc. 
+    ARCH_VH,
+    // @todo newer cpu targets like the H, J, R, and V series go here
+};
+
+// support for the numerics architecture (FPU)
+#define I960_FEATURE_NUMERICS (1 << 0) 
+// support for the protected architecture (MMU + OS and string instructions)
+#define I960_FEATURE_PROTECTED (1 << 1)
+// use complex addressing modes
+#define I960_FEATURE_COMPLEX (1 << 2)
+
+
 #endif
